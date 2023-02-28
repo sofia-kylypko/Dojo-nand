b75a5f0cfb065015aa768a31453ebf9ce48f1e96
Sofiia Kylypko <sofia.kilipko@gmail.com>
2023-02-28 15:28:58 +0000

    {

--- program.js
+++ program.js
@@ -1,38 +1,65 @@
+// CONSTANTS
+
+let CANVAS_WIDTH=800;
+let CANVAS_HEIGHT=600;
+let NANONAUT_WIDTH=181;
+let NANONAUT_HEIGHT=229;
+let GROUND_Y=540;
+
+// SETUP
+
 let canvas=document.createElement('canvas');
-canvas.width=800;
-canvas.height=600;
+let c = canvas.getContext('2d');
+canvas.width=CANVAS_WIDTH;
+canvas.height=CANVAS_HEIGHT;
 document.body.appendChild(canvas);
 
-let c=canvas.getContext('2d');
+let nanonautImage=new Image();
+nanonautImage.src='images/nanonaut.png';
 
-let image=new Image();  //creating object
-image.src='images/nanonaut.png';
+let backgroundImage=new Image();
+backgroundImage.src='images/background.png';
 
-//points-keys from where to start drawing animation
-let x=0;
-let y=40;
+let nanonautX=50;
+let nanonautY=40;
 
-window.addEventListener("load", start);
+window.addEventListener('load', start);
 
 function start(){
-    // c.fillStyle='green';
-    // //coordinates of area to fill
-    // c.fillRect(10,10,30,30); 
-
-    // //object and coordinates where to put it 
-    // c.drawImage(image,20,40);  
-
-    window.requestAnimationFrame(loop);
+    window.requestAnimationFrame(mainLoop);
 }
 
-function loop(){
-    //following will clear unnecessary and only show moved one
-    c.clearRect(0,0,800,600);
-    //drawing code will go there
-    c.drawImage(image, x, y)
-    x+=1;
-    //comand to draw intentions
-    window.requestAnimationFrame(loop);
+//MAIN LOOP
+
+function mainLoop(){
+    update();
+    draw();
+    window.requestAnimationFrame(mainLoop);
 }
 
-document.body.appendChild(canvas);
+//PLAYER INPUT
+//UPDATING
+
+function update(){
+    //update man and gravity
+    nanonautY+=1;
+    //part for figure not to go down from visible area
+    if(nanonautY>(GROUND_Y-NANONAUT_HEIGHT)){
+        nanonautY=GROUND_Y-NANONAUT_HEIGHT;
+    }
+}
+
+//DRAWING
+
+function draw(){
+    c.clearRect(0, 0 , CANVAS_WIDTH, CANVAS_HEIGHT);
+    //draw the background: sky, ground 
+    c.fillStyle='LightSkyBlue';
+    c.fillRect(0,0,CANVAS_WIDTH,GROUND_Y-40);
+    c.fillStyle='ForestGreen';
+    c.fillRect(0, GROUND_Y-40, CANVAS_WIDTH,CANVAS_HEIGHT-GROUND_Y+40);
+    //draw background
+    c.drawImage(backgroundImage,0,-210);
+    //draw the man figure
+    c.drawImage(nanonautImage, nanonautX, nanonautY);
+}
